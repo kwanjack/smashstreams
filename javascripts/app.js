0563a60acd2459f702ffb549123f3e8b7754cf4c
kk2385 <kk2385@nyu.edu>
2016-01-22 01:04:49 -0800

    forgot that in async you gotta put everything in call back

--- javascripts/app.js
+++ javascripts/app.js
@@ -1,19 +1,19 @@
 (function app() {
   var channels = ['hysranai', 'abadango', 'mew2king'];
 
-  function isLive(username) {
-    $.getJSON('https://api.twitch.tv/kraken/streams/' + username, function(channel) {
-      return channel.stream !== null;
-    });
-  }
-
   function addRow(username) {
-    var row = document.createElement('div');
-    var liveMessage = isLive(username)? " is live!" : " is offline.";
-    var rowHtml = '<a href=http://www.twitch.tv/' + username +'> ' + username + '</a>' + liveMessage;
-    row.innerHTML = rowHtml;
-    var body = document.getElementsByTagName('body')[0];
-    body.appendChild(row);
+    $.getJSON('https://api.twitch.tv/kraken/streams/' + username, function(channel) {
+      var isLive = channel.stream !== null;
+      console.log(isLive, channel.stream);
+      var row = document.createElement('div');
+      var liveMessage = isLive? " is live!" : " is offline.";
+      var rowHtml = '<a href=http://www.twitch.tv/' + username +'> ' + username + '</a>' + liveMessage;
+      row.innerHTML = rowHtml;
+      var body = document.getElementsByTagName('body')[0];
+      body.appendChild(row);
+    });
+
+
   }
 
   function generate() {
@@ -21,7 +21,5 @@
       addRow(channel);
     });
   }
-
   generate();
-  console.log('done');
 })();
